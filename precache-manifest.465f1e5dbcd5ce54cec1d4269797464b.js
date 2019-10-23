bcb6da307512b70d91b24ad2a5b26cbfea959ad0
Nick Place <nickjplace@gmail.com>
2019-10-22 23:53:40 -0600

    Updates

---
+++ precache-manifest.465f1e5dbcd5ce54cec1d4269797464b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f76860bff1cf5c0b1797d0cb713f392",
+    "url": "/reptar/index.html"
+  },
+  {
+    "revision": "fdf22e4b93a177931eef",
+    "url": "/reptar/static/css/main.4a65beb0.chunk.css"
+  },
+  {
+    "revision": "a2ef87b9ac94ded5a441",
+    "url": "/reptar/static/js/2.c4de058b.chunk.js"
+  },
+  {
+    "revision": "fdf22e4b93a177931eef",
+    "url": "/reptar/static/js/main.2bde6a0f.chunk.js"
+  },
+  {
+    "revision": "e333fb7f1b4d01e3a4d0",
+    "url": "/reptar/static/js/runtime-main.ef1ba158.js"
+  },
+  {
+    "revision": "1634cc0308f524b8252d1a8afa89f258",
+    "url": "/reptar/static/media/Circular-Book.1634cc03.woff"
+  },
+  {
+    "revision": "3f95dc1ec979880743462c0db8ca8eb8",
+    "url": "/reptar/static/media/Circular-Book.3f95dc1e.woff2"
+  },
+  {
+    "revision": "c62daec22417c3b01169ae970d4b4460",
+    "url": "/reptar/static/media/Recoleta-Black.c62daec2.woff"
+  },
+  {
+    "revision": "eb549c87c7c9febd8ce6b656b33a0f4d",
+    "url": "/reptar/static/media/Recoleta-Black.eb549c87.woff2"
+  },
+  {
+    "revision": "5cba621102d95a71453d3110a94d3db7",
+    "url": "/reptar/static/media/Recoleta-Medium.5cba6211.woff"
+  },
+  {
+    "revision": "ebdfbce1153d463bf7a1346dd549018c",
+    "url": "/reptar/static/media/Recoleta-Medium.ebdfbce1.woff2"
+  },
+  {
+    "revision": "2ad945cb8c3d6f7704b689cc8a571249",
+    "url": "/reptar/static/media/Recoleta-Regular.2ad945cb.woff2"
+  },
+  {
+    "revision": "6f1a84f3cd7f95ab2bb972ea54d4df20",
+    "url": "/reptar/static/media/Recoleta-Regular.6f1a84f3.woff"
+  },
+  {
+    "revision": "185d91707ff1b55b1fce6f50a367667c",
+    "url": "/reptar/static/media/logo-white.185d9170.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/reptar/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "3054a09a2b31022b310b7208605f0d67",
+    "url": "/reptar/static/media/steps-bg.3054a09a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reptar-demo/precache-manifest.0dce896313d8a0be3cafee3d1d3b6f5b.js"
+  "/reptar/precache-manifest.465f1e5dbcd5ce54cec1d4269797464b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reptar-demo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reptar/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
